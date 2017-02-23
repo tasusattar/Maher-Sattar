f849c19963e465afd6f39b4a463cba60bdbfa4f0
Tasawar Sattar <tasawar.sattar@mail.utoronto.ca>
2017-02-23 04:07:09 -0500

    polished the parallax a little
    tidied parallax. bugs on responsiveness. moving on

---
+++ js/video.js
@@ -0,0 +1,29 @@
+// function playvid(){
+//   document.getElementById('video').innerHTML = "<iframe src=\"https://www.youtube.com/embed/xTxSwy0pwTI?autoplay=1\" allowfullscreen=\"allowfullscreen\"></iframe>"
+// }
+
+// // var container = document.getElementById("grp3")
+// // var video = document.getElementById("showreel"),
+// // fraction = 0.8;
+// //
+// // function checkScroll() {
+// //     var x = container.offsetLeft, y = container.offsetTop, w = container.offsetWidth, h = container.offsetHeight, r = x + w, //right
+// //     b = y + h, //bottom
+// //     visibleX, visibleY, visible;
+// //
+// //     visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+// //     visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+// //
+// //     visible = visibleX * visibleY / (w * h);
+// //
+// //     if (visible > fraction) {
+//           document.getElementById('video').innerHTML = "<iframe src=\"https://www.youtube.com/embed/xTxSwy0pwTI?autoplay=1\" allowfullscreen=\"allowfullscreen\"></iframe>"
+// //         video.play();
+// //     } else {
+// //         video.pause();
+// //     }
+// //   }
+// // }
+// //
+// // window.addEventListener('scroll', checkScroll, false);
+// // window.addEventListener('resize', checkScroll, false);
